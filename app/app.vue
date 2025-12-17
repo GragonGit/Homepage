9d7e1eedf92d3c78fed2f6dfe67c3fb85a9c6d73
Gragon <gragonator02@gmail.com>
2025-12-17 15:39:14 +0100

    Added sidenav layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
   <main>
-    <NuxtPage />
+    <div class="layout">
+      <SideNav />
+      <NuxtPage />
+    </div>
   </main>
 </template>
 
@@ -26,4 +29,21 @@ main
     
     z-index: 9999
     pointer-events: none
+
+.layout
+  --min-sidebar-width: 10rem
+  --max-sidebar-width: 15rem
+  --grid-gap: 2rem
+
+  display: grid
+  grid-template-columns: minmax(var(--min-sidebar-width), var(--max-sidebar-width)) 1fr
+  gap: clamp(0dvw, 2dvw, var(--grid-gap))
+
+  @media (max-width: 600px)
+    grid-template-columns: 1fr
+    gap: 0
+  
+  > :last-child
+    justify-self: center
+    width: clamp(0ch, 100%, 70ch)
 </style>
---
+++ app/components/SideNav.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Projects</li>
+      <li>Games</li>
+      <li>Blog</li>
+      <li>Imprint</li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="ts"></style>
