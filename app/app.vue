76d1c32010bdae4007d64e53e4975e3aaf67a7cb
Gragon <gragonator02@gmail.com>
2025-12-21 22:52:29 +0100

    Added unnecessary almost invisible border effect

--- app/app.vue
+++ app/app.vue
@@ -15,6 +15,7 @@
 main
   padding: 2rem
 
+// Border
 .layout::before
     content: ''
     position: fixed
@@ -24,13 +25,42 @@ main
     border-radius: 5rem
 
     background: linear-gradient(to bottom, var(--accent-gradient)) border-box
-    /* This masks out the center area defined by the padding-box */
+    background-size: 100% 200%
+
+    animation: lava-lamp 12s cubic-bezier(0.45, 0.05, 0.55, 0.95) infinite alternate
+
+    // This masks out the center area defined by the padding-box
     -webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0)
     -webkit-mask-composite: destination-out
     mask-composite: exclude
     
     z-index: 9999
     pointer-events: none
+
+// Blur
+.layout::after
+    content: ''
+    position: fixed
+    
+    inset: -30px
+    border: 50px solid transparent
+    border-radius: 5rem
+
+    background: linear-gradient(black, black) padding-box, linear-gradient(to bottom, var(--accent-gradient)) border-box
+    background-size: 100% 200%
+
+    filter: blur(1rem)
+    animation: lava-lamp 12s cubic-bezier(0.45, 0.05, 0.55, 0.95) infinite alternate
+    opacity: 25%
+
+    z-index: -9999
+    pointer-events: none
+
+@keyframes lava-lamp
+  0%
+    background-position: 0% 0%
+  100%
+    background-position: 0% 100%
 
 .imprint
   display: flex
