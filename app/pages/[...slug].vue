efcc4b98c803eab2ce88ec4f0739fefba86bd551
Gragon <gragonator02@gmail.com>
2025-12-27 17:38:07 +0100

    Added page not found page

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,5 +1,10 @@
 <template>
   <ContentRenderer v-if="content" class="content" :value="content" />
+  <div v-else class="content">
+      <h1>Page Not Found</h1>
+      <p>Oops! The content you're looking for doesn't exist.</p>
+      <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,9 +12,9 @@ const route = useRoute()
 const path = computed(() => route.path)
 
 const { data: content } = await useAsyncData(
-    `content-${path.value}`,
-    () => queryCollection('content').path(path.value).first(),
-    { watch: [path] }
+  `content-${path.value}`,
+  () => queryCollection('content').path(path.value).first(),
+  { watch: [path] }
 )
 
 useTitle().value = content.value?.title ?? 'Page not found'
