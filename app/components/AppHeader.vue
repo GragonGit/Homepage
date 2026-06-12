696c05846d104a31e3f39e82550d3dcc85b96818
Gragon <gragonator02@gmail.com>
2026-06-12 16:14:04 +0200

    Implemented Header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,13 +1,37 @@
 <template>
-  <header>
-    Header
-  </header>
+<header>
+  <NuxtLink :to="link" v-for="link in links">{{ link }}</NuxtLink>
+</header>
 </template>
 
 <script lang="ts" setup>
-
+const links = ["home", "projects", "about"]
 </script>
 
 <style lang="sass" scoped>
+header
+  position: fixed
+  width: 100%
 
+  padding: 0.5rem
+
+  display: flex
+  justify-content: center
+  gap: 0.5em
+
+  mix-blend-mode: difference
+
+a
+  padding: 0.5em 1em
+  border-radius: 3em
+
+  font-size: 7pt
+  font-weight: bold
+  text-transform: uppercase
+
+  color: #fff
+
+  &:hover, &:focus-visible
+    background-color: var(--background)
+    color: #000
 </style>
