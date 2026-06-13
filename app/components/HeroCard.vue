161218d810a114ea175e944c40ae4195a059e67e
Gragon <gragonator02@gmail.com>
2026-06-14 01:23:55 +0200

    Hero section cards

---
+++ app/components/HeroCard.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="card">
+  <Icon :name="icon" />
+  <NuxtLink :to="url">{{ label }}</NuxtLink>
+  <label>{{ label }}</label>
+</div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  label: string
+  url: string
+  icon: string
+}>()
+</script>
+
+<style lang="sass" scoped>
+.card
+  position: relative
+  aspect-ratio: 1
+
+  border: solid 10px var(--secondary) 
+  border-radius: 1em
+
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: stretch
+  gap: 0.5rem
+
+  padding: 1em
+
+  background-color: var(--primary)
+
+  &:hover, &:focus-within, &:focus-visible
+    background-color: var(--secondary)
+
+    .iconify
+      color: var(--primary)
+
+    // 3. ...and change the child label color
+    label
+      color: var(--primary)
+
+a
+  position: absolute
+  inset: 0
+  font-size: 0
+
+  z-index: 2
+
+.iconify
+  width: auto
+  height: 0
+  flex-grow: 1
+  aspect-ratio: 1
+
+  color: var(--secondary)
+
+label
+  flex-shrink: 0
+
+  text-transform: uppercase
+  color: var(--secondary)
+</style>
--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -2,17 +2,30 @@
 <section class="hero content">
   <h1>Tristan</h1>
   <p id="professions">Web Games Software</p>
+  <ul>
+    <li v-for="card in cards" :key="card.label">
+      <HeroCard v-bind="card" />
+    </li>
+  </ul>
 </section>
 </template>
+
+<script lang="ts" setup>
+const cards = [
+  { label: "projects", url: "projects", icon: "material-symbols:web" },
+  { label: "games", url: "games", icon: "material-symbols:stadia-controller" },
+  { label: "skills", url: "skills", icon: "material-symbols:star-rounded" },
+  { label: "about", url: "about", icon: "material-symbols:person" }
+]
+</script>
 
 <style lang="sass" scoped>
 .hero
   margin-top: 5rem
   max-width: 50rem
 
-
 h1
-  margin-block: 0
+  margin-block: 2rem
 
   display: flex
   align-items: baseline
@@ -34,7 +47,6 @@ h1
     background-image: repeating-linear-gradient(45deg, var(--secondary), var(--secondary) 20px, transparent 20px, transparent 40px)
 
 #professions
-  margin-block: 1rem
   padding: 0.25rem 1rem
 
   border-radius: 3em
@@ -44,4 +56,14 @@ h1
 
   background-color: var(--secondary)
   color: var(--primary)
+
+ul
+  margin: 3rem 0
+  padding: 0
+
+  display: grid
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr))
+  gap: 1.5rem
+
+  list-style-type: none
 </style>
