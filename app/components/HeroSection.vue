a2d4401e9f254cc0c485ab3c484529896a15ef3e
Gragon <gragonator02@gmail.com>
2026-06-15 00:56:47 +0200

    Haha funny bg

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,5 +1,7 @@
 <template>
-<section>
+<section @mousemove="updateMouseCoords">
+  <div class="matrix-bg" :style="mouseStyle" />
+
   <div class="hero content">
     <h1>Gragon</h1>
     <p id="subheader"><span>Game & Software Engineer</span> <span>Germany</span></p>
@@ -21,6 +23,19 @@ const cards = [
   { label: "skills", url: "skills", icon: "material-symbols:star-sharp" },
   { label: "about", url: "about", icon: "material-symbols:person-sharp" }
 ]
+
+const mouseX = ref(0)
+const mouseY = ref(0)
+
+const updateMouseCoords = (e: MouseEvent) => {
+  mouseX.value = e.pageX
+  mouseY.value = e.pageY
+}
+
+const mouseStyle = computed(() => ({
+  '--mouse-x': `${mouseX.value}px`,
+  '--mouse-y': `${mouseY.value}px`
+}))
 </script>
 
 <style lang="sass" scoped>
@@ -28,13 +43,30 @@ section
   position: relative
 
   min-height: 100lvh
-  
+
   border-bottom: 16px solid var(--secondary)
 
   display: flex
   flex-direction: column
   align-items: stretch
   justify-content: center
+
+.matrix-bg
+  --bg-size: 40px
+
+  position: absolute
+  inset: 0
+  pointer-events: none
+
+  background: radial-gradient(circle, transparent 5px, var(--primary) 5px), linear-gradient(90deg, var(--secondary) 2px, transparent 2px), linear-gradient(0deg, var(--secondary) 2px, transparent 2px)
+  background-position: calc( calc( var(--bg-size) / 2 ) + 1px ) calc( calc( var(--bg-size) / 2 ) - 1px ), 0 0, 0 0
+  background-size: var(--bg-size) var(--bg-size)
+  opacity: 0.2
+
+  mask-image: radial-gradient(circle 100px at var(--mouse-x) var(--mouse-y), black 10%, #0004 100%)
+  -webkit-mask-image: radial-gradient(circle 100px at var(--mouse-x) var(--mouse-y), black 10%, #0002 100%)
+
+  z-index: -10
 
 #scroll-indicator
   position: absolute
