79043fac536ca0cfe98acf6a8d6479e0e55ac341
Gragon <gragonator02@gmail.com>
2026-06-16 17:08:12 +0200

    Hide mouse effect in hero section on hero section exit

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,5 +1,5 @@
 <template>
-<section id="hero" @mousemove="updateMouseCoords">
+<section id="hero" @mousemove="updateMouseCoords" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
   <div class="matrix-bg" :style="mouseStyle" />
 
   <div class="hero content">
@@ -19,6 +19,7 @@
 <script lang="ts" setup>
 const mouseX = ref(0)
 const mouseY = ref(0)
+const isHovered = ref(false)
 
 const updateMouseCoords = (e: MouseEvent) => {
   mouseX.value = e.pageX
@@ -27,7 +28,8 @@ const updateMouseCoords = (e: MouseEvent) => {
 
 const mouseStyle = computed(() => ({
   '--mouse-x': `${mouseX.value}px`,
-  '--mouse-y': `${mouseY.value}px`
+  '--mouse-y': `${mouseY.value}px`,
+  '--mask-opacity': isHovered.value ? '0.2' : '0'
 }))
 </script>
 
@@ -54,7 +56,9 @@ section
   background: radial-gradient(circle, transparent 5px, var(--primary) 5px), linear-gradient(90deg, var(--secondary) 2px, transparent 2px), linear-gradient(0deg, var(--secondary) 2px, transparent 2px)
   background-position: calc( calc( var(--bg-size) / 2 ) + 1px ) calc( calc( var(--bg-size) / 2 ) - 1px ), 0 0, 0 0
   background-size: var(--bg-size) var(--bg-size)
-  opacity: 0.2
+  
+  opacity: var(--mask-opacity, 0)
+  transition: opacity 0.3s ease
 
   mask-image: radial-gradient(circle 100px at var(--mouse-x) var(--mouse-y), black 10%, #0004 100%)
   -webkit-mask-image: radial-gradient(circle 100px at var(--mouse-x) var(--mouse-y), black 10%, #0002 100%)
